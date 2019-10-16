5e814700b1a99ab12d2ce0a39be943b297fffe64
Derick Tseng <derickboss1@gmail.com>
2019-10-16 02:27:10 -0700

    simplify build tree

--- build.bat
+++ build.bat
@@ -1,2 +1,8 @@
 @echo off
-call "build_utils/build.bat"
+pip install -r requirements.txt
+
+python "build_utils/get_sc2reader_path.py" > sc2reader
+SET /p sc2readerpath=<sc2reader
+DEL sc2reader
+
+pyinstaller --onefile --clean --windowed --noconfirm --name="SC2ReplayRenamer" --add-data="%sc2readerpath%;sc2reader" --add-data="src;src" run.py
